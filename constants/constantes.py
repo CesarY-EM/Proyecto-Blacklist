28378c69a515c66eafefbb9f8fe48aaf34e58188
Cesar EM <cesaryaem@gmail.com>
2026-06-18 11:21:06 -0600

    Ajuste de reglas de negocio en backend para validación de subredes

--- constants/constantes.py
+++ constants/constantes.py
@@ -1,24 +1,21 @@
 from pydnsbl.providers import Provider
 
 MAX_WORKERS = 20
-UMBRAL = 0.8
-
-MUESTRA_24 = 25
-MUESTRA_16 = 100
 
 PREFIJO_24 = 24
 PREFIJO_16 = 16
 
-PROVIDERS = [Provider('truncate.gbudb.net'),
-            Provider('spam.spamrats.com'),
-            Provider('dyna.spamrats.com'),
-            Provider('auth.spamrats.com'),
-            Provider('noptr.spamrats.com'),
-            Provider('zen.spamhaus.org'),
-            Provider('bl.spamcop.net'),
-            Provider('b.barracudacentral.org'),
-            Provider('dnsbl.sorbs.net'),
-            ]
+PROVIDERS = [
+    Provider('truncate.gbudb.net'),
+    Provider('spam.spamrats.com'),
+    Provider('dyna.spamrats.com'),
+    Provider('auth.spamrats.com'),
+    Provider('noptr.spamrats.com'),
+    Provider('zen.spamhaus.org'),
+    Provider('bl.spamcop.net'),
+    Provider('b.barracudacentral.org'),
+    Provider('dnsbl.sorbs.net'),
+]
 
 PESTANAS = {
     "BLOQUEO":   {"color": "FFB3B3", "columnas": ["Bloque", "Resultado"]},
